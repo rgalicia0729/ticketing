e12da3e6db09b0349c886447fa9c9900f1442b8b
Rigo Antonio Galicia Barrera <rigo.galicia@icloud.com>
2023-04-05 15:52:20 -0600

    The error strategy and connection to the database was finalized

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+require('express-async-errors');
+
+import mongoose from 'mongoose';
 
 import { CurrentUserRouter, SigninRouter, SignupRouter, SignoutRouter } from './routes';
 import { errorHandler } from './middlewares';
+import { NotFoundError } from './errors';
 
 const PORT = 3000;
 const app = express();
@@ -14,8 +18,26 @@ app.use(SigninRouter);
 app.use(SignoutRouter);
 app.use(CurrentUserRouter);
 
+app.all('*', () => {
+    throw new NotFoundError();
+});
+
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-    console.log(`Auth service running on port ${PORT}`);
-});
+// Connect DB
+const start = async () => {
+    try {
+        mongoose.set('strictQuery', false);
+        await mongoose.connect('mongodb+srv://coursesusr:o2CtZBAFII5WRqtP@courses.6gjsk7h.mongodb.net/auth?retryWrites=true&w=majority');
+
+        console.log('DB online');
+    } catch (err) {
+        console.error(err);
+    }
+
+    app.listen(PORT, () => {
+        console.log(`Auth service running on port ${PORT}`);
+    });
+}
+
+start();
