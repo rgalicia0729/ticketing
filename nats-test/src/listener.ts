87cfc4e59799b2cbf5179d4b0b4d98f9b74f42a2
Rigo Antonio Galicia Barrera <rigo.galicia@icloud.com>
2023-04-30 08:39:58 -0600

    Improving the NATS listener

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,5 +1,7 @@
-import nats, { Message } from 'node-nats-streaming';
+import nats from 'node-nats-streaming';
 import { randomBytes } from 'crypto';
+
+import { TicketCreatedListener } from './events/ticket-created-listener';
 
 console.clear();
 
@@ -15,23 +17,8 @@ stan.on('connect', () => {
         process.exit();
     });
 
-    const options = stan
-        .subscriptionOptions()
-        .setManualAckMode(true)
-        .setDeliverAllAvailable()
-        .setDurableName('accounting-server');
+    new TicketCreatedListener(stan).listen();
 
-    const subscription = stan.subscribe('ticket:created', 'queue-group-name', options);
-
-    subscription.on('message', (msg: Message) => {
-        const data = msg.getData();
-
-        if (typeof data === 'string') {
-            console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
-        }
-
-        msg.ack();
-    });
 });
 
 process.on('SIGINT', () => stan.close());
