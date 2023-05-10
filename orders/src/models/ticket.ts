07d1a9b9ca39e1987648ec09266676f8cb1469b5
Rigo Antonio Galicia Barrera <rigo.galicia@icloud.com>
2023-05-10 08:26:57 -0600

    Added version validation in ticket and order for events

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 import { OrderStatus } from '@rg-ticketing/common';
 
 import { Order } from './';
@@ -12,11 +13,13 @@ interface TicketAttrs {
 export interface TicketDoc extends mongoose.Document {
     title: string;
     price: number;
+    version: number;
     isReserved(): Promise<boolean>;
 }
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
     build(attrs: TicketAttrs): TicketDoc;
+    findByEvent(event: { id: string, version: number }): Promise<TicketDoc | null>;
 }
 
 const ticketSchema = new mongoose.Schema(
@@ -41,11 +44,21 @@ const ticketSchema = new mongoose.Schema(
     }
 );
 
+ticketSchema.set('versionKey', 'version');
+ticketSchema.plugin(updateIfCurrentPlugin);
+
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
     return new Ticket({
         _id: attrs.id,
         title: attrs.title,
         price: attrs.price
+    });
+}
+
+ticketSchema.statics.findByEvent = (event: { id: string, version: number }): Promise<TicketDoc | null> => {
+    return Ticket.findOne({
+        _id: event.id,
+        version: event.version - 1
     });
 }
 
